df880a4346da0d1eb84a8e30439ed0d50e74a22e
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-04 09:48:56 +0600

    link for each chef created

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
 
+app.get('/chefs/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    console.log(id);
+    const chef = chefs.find(ch => ch.id === id);
+    res.send(chef)
+})
+
 app.listen(port, () => {
     console.log('chef server');
 })
